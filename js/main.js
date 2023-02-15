779d3aff0af137b75a49951d686ab5d1cff0d9ca
VladiSlave <sukhovey.vlad@gmail.com>
2023-02-15 19:07:37 +1000

    clean up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const sizeWindow = document.getElementById('size-window')
     const sizeToggle = document.querySelectorAll('.size-toggle')
 
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const sizeButtons = document.querySelector('.size-buttons').childNodes
     const selectButton = document.querySelector('.size-button-choose')
 
-    // console.log(selectButton)
+
     sizeButtons.forEach((button) => {
         button.addEventListener('click', () => {
             button.classList.toggle('is-active')
@@ -23,15 +24,17 @@ document.addEventListener('DOMContentLoaded', function() {
     let sizeNumbers = 0
     let sizeMass = []
     let buttonCell
-    const sidebarSizes = '<button class="sidebar-size-button"></button>'
 
     sizeButtons.forEach((button) => {
         button.addEventListener('click', () => {
             if (button.classList.contains('is-active') && !button.classList.contains('selected')) {
-                button.classList.add('selected')
                 sizeNumbers += 1
+
+                button.classList.add('selected')
+
                 buttonCell = button.outerHTML
                 buttonCell.className = 'alert'
+
                 sizeMass.push(button.outerHTML)
             }
         })
@@ -42,14 +45,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const addButton = '<div class="workspace-button-add flex"><button> <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M0 15H30.5" stroke="#092F63" stroke-width="1.5"/> <path d="M15.5 0V30" stroke="#092F63" stroke-width="1.5"/> </svg><span>добавить фото</span> </button></div>'
 
     selectButton.addEventListener('click', () => {
-        console.log('pirate')
+
         sizeWindow.classList.toggle('is-active')
         sidebarSizeButtons.classList.add('is-active')
+
         let numbersSizeButtons = sidebarSizeButtons.childNodes.length
+
         if((sizeNumbers > 0) && (!sidebarButton.classList.contains('is-inactive'))) {
             sidebarButton.classList.toggle('is-inactive')
             workspace.insertAdjacentHTML('beforeend', addButton)
-            console.log(sizeNumbers)
         }
 
         function appendButtons() {
@@ -60,9 +64,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const cleanStack = async () => {
             await appendButtons()
+
             sizeNumbers = 0
             sizeMass = []
-            console.log(sizeNumbers, "djsdkuku")
         }
 
         cleanStack()
