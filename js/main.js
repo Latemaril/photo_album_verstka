4e465f075d69bc0eaf7ea8060ea3f34f9e179d84
VladiSlave <sukhovey.vlad@gmail.com>
2023-02-23 18:05:06 +1000

    pizdec

--- js/main.js
+++ js/main.js
@@ -16,9 +16,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const downloadWorkspacePhotos = document.querySelectorAll('.js-download-workspace-photo')
     const downloadButtonAdd = document.querySelector('.js-download-add')
     const workspacePhotosBlocks = document.querySelectorAll('.js-workspace-block')
+    const mainPhotoContainer = '                    <div class="photos-container vertical">\n' +
+        '                        <div class="photo">\n' +
+        '                            <div class="photo-footer"></div>\n' +
+        '                        </div>\n' +
+        '                    </div>'
     let sizeNumbers = 0
     let dataSizeButtons = []
     let dataSizeBlocks = []
+    let dataPhotos = []
 
     for (let i = 0; i < workspacePhotosBlocks.length; i++) {
         dataSizeBlocks.push(workspacePhotosBlocks[i].getAttribute('data-size'))
@@ -86,7 +92,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             document.querySelector('.js-workspace-button-select-all').classList.toggle('is-visible')
             document.querySelector('.js-add-photo-mini').classList.toggle('is-active')
-            console.log('hehehaha')
         }
 
         const cleanStack = async () => {
@@ -95,7 +100,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         cleanStack().then(() => {
-            console.log('completed')
         })
 
     })
@@ -103,13 +107,32 @@ document.addEventListener('DOMContentLoaded', function () {
     underHeaderSizeCloseButtons.forEach((close) => {
         close.addEventListener('click', () => {
             let buttonsSizeSelected = document.querySelectorAll('.js-selected')
-            for (let j = 0; j < buttonsSizeSelected.length; j++) {
-                if (close.parentElement.getAttribute('data-size') === buttonsSizeSelected[j].getAttribute('data-size')) {
-                    close.parentElement.classList.toggle('is-inactive')
-                    buttonsSizeSelected[j].classList.remove( 'js-selected')
-                    buttonsSizeSelected[j].classList.toggle('is-froze')
-                    buttonsSizeSelected[j].classList.toggle( 'is-active')
-                    console.log('cucu')
+            if (!document.querySelector('.js-workspace-block.js-active')) {
+                for (let j = 0; j < buttonsSizeSelected.length; j++) {
+                    if (close.parentElement.getAttribute('data-size') === buttonsSizeSelected[j].getAttribute('data-size')) {
+                        close.parentElement.classList.toggle('is-inactive')
+                        buttonsSizeSelected[j].classList.remove( 'js-selected')
+                        buttonsSizeSelected[j].classList.toggle('is-froze')
+                        buttonsSizeSelected[j].classList.toggle( 'is-active')
+                    }
+                }
+            }
+            else {
+                for (let j = 0; j < buttonsSizeSelected.length; j++) {
+                    if (close.parentElement.getAttribute('data-size') === buttonsSizeSelected[j].getAttribute('data-size')) {
+                        close.parentElement.classList.toggle('is-inactive')
+                        buttonsSizeSelected[j].classList.remove( 'js-selected')
+                        buttonsSizeSelected[j].classList.toggle('is-froze')
+                        buttonsSizeSelected[j].classList.toggle( 'is-active')
+                        console.log('MAMA')
+                        for (let i = 0; i < dataSizeBlocks.length; i++) {
+                            if ((close.parentElement.getAttribute('data-size') === workspacePhotosBlocks[i].getAttribute('data-size')) && close.parentElement.classList.contains('js-filled')) {
+                                close.parentElement.classList.remove('js-filled')
+                                workspacePhotosBlocks[i].classList.remove('is-active')
+                                workspacePhotosBlocks[i].classList.remove('js-active')
+                            }
+                        }
+                    }
                 }
             }
         })
@@ -117,26 +140,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
     underHeaderSizeButtonsActive.forEach((select) => {
         select.addEventListener('click', () => {
-            if (document.querySelector('.is-selected') && !select.classList.contains('is-selected')) {
+            if (!document.querySelector('.js-workspace-block.js-active')) {
+                if (document.querySelector('.js-size.is-selected') && !select.classList.contains('is-selected')) {
 
-                function tt() {
-                    document.querySelector('.is-selected').classList.toggle('is-selected')
-                    document.querySelector('.workspace-button-big').classList.toggle('is-active')
+                    function tt() {
+                        document.querySelector('.is-selected').classList.toggle('is-selected')
+                        document.querySelector('.workspace-button-big').classList.toggle('is-active')
+                    }
+                    const cc = async () => {
+                        await tt()
+                        select.classList.toggle('is-selected')
+                        document.querySelector('.workspace-button-big').classList.toggle('is-active')
+                    }
+
+                    cc().then(() => {
+                        console.log('completed')
+                    })
+                    console.log('find')
                 }
-                const cc = async () => {
-                    await tt()
+                else {
+                    console.log('ban')
                     select.classList.toggle('is-selected')
                     document.querySelector('.workspace-button-big').classList.toggle('is-active')
                 }
-
-                cc().then(() => {
-                    console.log('completed')
-                })
-                console.log('find')
-            }
-            else {
-                select.classList.toggle('is-selected')
-                document.querySelector('.workspace-button-big').classList.toggle('is-active')
             }
         })
     })
@@ -156,18 +182,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 function tt() {
                     document.querySelector('.js-storage-selected').classList.toggle('is-active')
                     document.querySelector('.js-storage-selected').classList.toggle('js-storage-selected')
-
-                    console.log('1')
                 }
                 const cc = async () => {
                     await tt()
                     select.classList.toggle('js-storage-selected')
                     select.classList.toggle('is-active')
-                    console.log('2')
                 }
 
                 cc().then(() => {
-                    console.log('completed')
                 })
             }
             // else {
@@ -196,18 +218,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 photo.classList.toggle('is-selected')
                 photo.classList.toggle('js-selected')
                 count += 1
-                console.log(count)
             }
             else {
                 photo.classList.toggle('is-selected')
                 photo.classList.toggle('js-selected')
                 count -= 1
-                console.log(count)
             }
 
             if (count === 1) {
                 document.querySelector('.js-download-add').classList.add('is-active')
-                console.log('wtf')
             }
             else if (count === 0) {
                 document.querySelector('.js-download-add').classList.remove('is-active')
@@ -218,13 +237,107 @@ document.addEventListener('DOMContentLoaded', function () {
 
     downloadButtonAdd.addEventListener('click', () => {
         console.log('click')
-        for (let i = 0; i < dataSizeBlocks.length; i++) {
-            if (document.querySelector('.js-size.is-selected').getAttribute('data-size') === workspacePhotosBlocks[i].getAttribute('data-size')) {
-                workspacePhotosBlocks[i].classList.add('is-active')
-                workspaceBigButton.classList.toggle('is-active')
-                downloadWindow.classList.toggle('is-active')
+        let photos = document.querySelectorAll('.js-photo')
+
+
+        function first() {
+            for (let i = 0; i < photos.length; i++) {
+                if (photos[i].parentElement.classList.contains('js-selected')) {
+                    dataPhotos.push(photos[i])
+                }
+            }
+            console.log(dataPhotos)
+
+            for (let i = 0; i < dataSizeBlocks.length; i++) {
+                if (document.querySelector('.js-size.is-selected').getAttribute('data-size') === workspacePhotosBlocks[i].getAttribute('data-size')) {
+                    workspacePhotosBlocks[i].classList.toggle('is-active')
+                    workspacePhotosBlocks[i].classList.add('js-active')
+                    workspaceBigButton.classList.toggle('is-active')
+                    downloadWindow.classList.toggle('is-active')
+                    document.querySelector('.js-size.is-selected').classList.toggle('js-filled')
+
+                    for (let j = 0; j < dataPhotos.length; j++) {
+
+                        function tt() {
+                            workspacePhotosBlocks[i].insertAdjacentHTML('afterbegin', mainPhotoContainer)
+                        }
+                        const cc = async () => {
+                            await tt()
+
+                            for (let g = 0; g < dataSizeBlocks.length; g++) {
+                                console.log('Zaebal tvar')
+                                if (document.querySelector('.js-size.is-selected').getAttribute('data-size') === workspacePhotosBlocks[i].getAttribute('data-size')) {
+                                    workspacePhotosBlocks[i].getElementsByClassName('photo')[g].insertAdjacentHTML('afterbegin', String(dataPhotos[g].outerHTML))
+                                    console.log( workspacePhotosBlocks[i].getElementsByClassName('js-photo')[g])
+                                }
+
+                            }
+                            console.log("vot photo")
+                        }
+
+                        cc().then(() => {
+                        })
+
+                    }
+                }
             }
         }
+        const second = async () => {
+            await first()
+            dataPhotos = []
+            for (let i = 0; i < photos.length; i++) {
+                photos[i].parentElement.classList.remove('js-selected')
+                photos[i].parentElement.classList.remove('is-selected')
+            }
+        }
+
+        second().then(() => {
+        })
+
+    })
+
+    underHeaderSizeButtonsActive.forEach((select) => {
+        select.addEventListener('click', () => {
+            if (document.querySelector('.js-workspace-block.js-active')) {
+                console.log('PARAM')
+                if (!select.classList.contains('js-filled')) {
+                    document.querySelector('.is-selected').classList.toggle('is-selected')
+                    select.classList.toggle('is-selected')
+                    document.querySelector('.js-workspace-block.is-active').classList.toggle('is-active')
+                    workspaceBigButton.classList.toggle('is-active')
+                    console.log('Ledy')
+                }
+                else if ((select.classList.contains('js-filled')) && (!select.classList.contains('is-selected') && (!document.querySelector('.js-size.is-selected.js-filled')))) {
+                    document.querySelector('.js-size.is-selected').classList.toggle('is-selected')
+                    select.classList.toggle('is-selected')
+                    console.log('Zaebal mraz', dataSizeBlocks.length)
+                    for (let i = 0; i < dataSizeBlocks.length; i++) {
+                        console.log('Zaebal tvar')
+                        if (select.getAttribute('data-size') === workspacePhotosBlocks[i].getAttribute('data-size')) {
+                            workspacePhotosBlocks[i].classList.toggle('is-active')
+                            console.log('Zaebal gnida')
+                        }
+                    }
+                    workspaceBigButton.classList.toggle('is-active')
+                    console.log('Ledy GAGA')
+                }
+
+                else if ((select.classList.contains('js-filled')) && (!select.classList.contains('is-selected') && (document.querySelector('.js-size.is-selected.js-filled')))) {
+                    document.querySelector('.js-size.is-selected').classList.toggle('is-selected')
+                    document.querySelector('.js-workspace-block.is-active').classList.toggle('is-active')
+                    select.classList.toggle('is-selected')
+                    console.log('Zaebal mraz', dataSizeBlocks.length)
+                    for (let i = 0; i < dataSizeBlocks.length; i++) {
+                        console.log('Zaebal tvar')
+                        if (select.getAttribute('data-size') === workspacePhotosBlocks[i].getAttribute('data-size')) {
+                            workspacePhotosBlocks[i].classList.toggle('is-active')
+                            console.log('Zaebal gnida')
+                        }
+                    }
+                    console.log('Ledy GAGA')
+                }
+            }
+        })
     })
 
 })
